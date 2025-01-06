00a3c664351cc0514711e5ef4670114d821a4785
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2025-01-06 12:05:25 +0000

    Automatic commit before Assistant edits

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 
 module.exports = {
   plugins: {
-    'postcss-import': {},
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': false }
-    }
   }
 };
